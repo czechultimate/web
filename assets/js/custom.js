401e155e8a13f65ebbae278e152a16a61b3499fa
Marek Dostál <dstlmrk@gmail.com>
2020-04-14 15:25:45 +0200

    responsive menu

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -17,20 +17,20 @@ $(".navigation .menu .browse").popup({
     }
 });
 
-// $(document)
-//     .ready(function () {
-//         // fix menu when passed
-//         $('.masthead')
-//             .visibility({
-//                 once: false,
-//                 onBottomPassed: function () {
-//                     $('.fixed.menu').transition('fade in');
-//                 },
-//                 onBottomPassedReverse: function () {
-//                     $('.fixed.menu').transition('fade out');
-//                 }
-//             });
+$(document)
+    .ready(function () {
+        // fix menu when passed
+        $('.masthead')
+            .visibility({
+                once: false,
+                onBottomPassed: function () {
+                    $('.fixed.menu').transition('fade in');
+                },
+                onBottomPassedReverse: function () {
+                    $('.fixed.menu').transition('fade out');
+                }
+            });
 
-//         // create sidebar and attach to menu open
-//         $('.ui.sidebar').sidebar('attach events', '.toc.item');
-//     });
+        // create sidebar and attach to menu open
+        $('.ui.sidebar').sidebar('attach events', '.toc.item');
+    });
