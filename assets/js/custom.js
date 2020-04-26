38453700c4417a94c999f47ba7e74696853274c0
Marek Dostál <dstlmrk@gmail.com>
2020-04-26 15:30:48 +0200

    add dropdown navigation

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,10 +1,14 @@
 $(document).ready(function () {
-
     // striped tabulky kvuli markdown strankam
     $("table").addClass("ui striped celled table");
 
     // inicializace dropdownu
     $(".ui.dropdown").dropdown();
+
+    $(".dropdown-navigation").dropdown({
+        on: "hover",
+        clearable: true,
+    });
 
     // otevirani sidebaru ma mobilu
     $("#open-sidebar").click(function () {
